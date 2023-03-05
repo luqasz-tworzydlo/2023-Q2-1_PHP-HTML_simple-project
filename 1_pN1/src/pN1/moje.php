4bae333e6d2a24c05040702d236b88de259adc3c
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-03-05 21:47:05 +0100

    Add files via upload

---
+++ 1_pN1/src/pN1/moje.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+</head>
+<style>
+table {border:1px solid black; background-color:blue;}
+td {border:2px solid gold;
+padding:1px; width:20px; text-align:center;
+background-color:#999999;
+}
+</style>
+<body>
+<p> Hello from HTML </p>
+
+<!-- comment -->
+<img src ="wsb.png" alt ="cos tam"/>
+
+<?php
+
+// comment [one line]
+/* commented a little bit more */
+
+// echo "Hello from PHP";
+
+?>
+
+</body>
+</html>
