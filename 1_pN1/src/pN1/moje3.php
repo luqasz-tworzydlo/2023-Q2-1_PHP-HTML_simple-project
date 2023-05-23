41b595844fc039fa5365ba0e291e867eb5be4cda
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-05-24 00:09:51 +0200

    Add files via upload

--- 1_pN1/src/pN1/moje3.php
+++ 1_pN1/src/pN1/moje3.php
@@ -10,8 +10,8 @@ td { border:0px solid gold;
 padding:0px;  text-align:center;
 background-color:#999999;
 font-size:36px;
-width:10px;
-height:10px;
+width:50px;
+height:50px;
 }
 </style>
 
@@ -19,16 +19,53 @@ height:10px;
 <?php
 
 // inicjujemy tablicę wartościami losowymi
-for ($i=0; $i<10; $i++)
-for ($j=0; $j<10; $j++)
-$tab[$i][$j] = rand(100,255);
+for ($i=0; $i<10; $i++) {
+for ($j=0; $j<10; $j++) {
+$tab[$i][$j] = rand(0,255);
+}
+}
 
-// wyswietlenie tablicy poprzez kolory RGB
+// wyswietlenie tablicy poprzez kolory RGB [ przed stworzeniem sredniej kolorow ]
+echo "<p><h1>Tablica przed wybraniem sredniej kolorow</b></h1><p/>";
 echo "<table>";
 foreach ($tab as $wiersz) {
 echo "<tr>";
 foreach ($wiersz as $komorka) {
-$style = "style='background-color:rgb($komorka,$komorka,0)'";
+$style = "style='background-color:rgb(0,$komorka,0)'";
+echo "<td $style> </td>";
+}
+echo "</tr>";
+}
+echo "</table>";
+
+echo "<br/>";
+
+for ($i=0; $i<10; $i++){
+for($j=0; $j<10; $j++){
+$srednia_colorow = ($sum=$tab[$i][$j] +
+$sum=$tab[$i][$j+1] +
+$sum=$tab[$i][$j-1] +
+
+$sum=$tab[$i+1][$j] +
+$sum=$tab[$i+1][$j+1] +
+$sum=$tab[$i+1][$j-1] +
+
+$sum=$tab[$i-1][$j] +
+$sum=$tab[$i-1][$j+1] +
+$sum=$tab[$i-1][$j-1])/9;
+
+$tab[$i][$j] = $srednia_colorow;
+}
+}
+
+// wyswietlenie tablicy poprzez kolory RGB [ po stworzeniu sredniej kolorow ]
+echo "<p><h1>Tablica po wybraniu sredniej kolorow</b></h1><p/>";
+
+echo "<table>";
+foreach ($tab as $wiersz) {
+echo "<tr>";
+foreach ($wiersz as $komorka) {
+$style = "style='background-color:rgb(0,$komorka,0)'";
 echo "<td $style> </td>";
 }
 echo "</tr>";
