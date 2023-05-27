6bb7ebc215d1991feebde60ed1a1dbed8d591e75
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-05-28 00:14:09 +0200

    Add files via upload

--- 1_pN1/src/pN1/moje5_mapa-klikalnosci/moje5.4_mapa-obszar3.php
+++ 1_pN1/src/pN1/moje5_mapa-klikalnosci/moje5.4_mapa-obszar3.php
@@ -13,7 +13,7 @@
 
 	<p>Gratulacje! Odnalazłeś ręcznie malowany obraz nr 3!</p>
 
-  <img src="img/paiting-no1.jpg"  usemap="#mapka"/>
+  <img src="img/paiting-no3.jpg"  usemap="#mapka"/>
 
 	<p>Aby wrócić do gry kliknij: <a href="moje5.1_mapa-klik.php?item=OK">TUTAJ</a></p>
 	</body>
